ce405317be11530beb5815a54a9b78d568cfa559
Nimus-oes <sseo.code@gmail.com>
2025-06-18 08:36:42 +0900

    Create & set Not Found page

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import NotFound from "./pages/NotFound.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <NotFound />,
   },
 ]);
 
