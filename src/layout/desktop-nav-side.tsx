2d738546aefc0ce5571c798d0af8bcfcfc18c2eb
Nimus-oes <sseo.code@gmail.com>
2025-06-19 12:00:40 +0900

    Recreate project structure on a feature basis

---
+++ src/layout/desktop-nav-side.tsx
@@ -0,0 +1,7 @@
+export default function DesktopNavSide() {
+  return (
+    <div>
+      <p>Desktop Side Bar</p>
+    </div>
+  );
+}
---
+++ src/layout/mobile-nav-bottom.tsx
@@ -0,0 +1,3 @@
+export default function MobileNavBottom() {
+  return <div></div>;
+}
