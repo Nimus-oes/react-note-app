7032182fceec50f812ca837c2a1205b5fc722ec1
Nimus-oes <sseo.code@gmail.com>
2025-07-01 15:33:12 +0900

    Add create new note button for desktop/mobile screens (#4)

--- src/pages/note-list-page.tsx
+++ src/pages/note-list-page.tsx
@@ -1,7 +1,24 @@
+import { useIsMobile } from "../hooks/use-is-mobile";
 import SubLayout from "../layout/sub-layout";
 
 export default function NoteListPage() {
-  const noteList = <h1>Note List</h1>;
+  const isMobile = useIsMobile();
+
+  const mobileLayout = (
+    <>
+      <h1>Note List</h1>
+      <div>+</div>
+    </>
+  );
+
+  const desktopLayout = (
+    <>
+      <div>Create New Note</div>
+      <h1>Note List</h1>
+    </>
+  );
+
+  const noteList = isMobile ? mobileLayout : desktopLayout;
 
   return (
     <>
