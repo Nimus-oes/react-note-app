cb05fa7a53904ee460bb3cefb7cd71b025bed9f2
Nimus-oes <sseo.code@gmail.com>
2025-07-01 15:57:40 +0900

    Define Note and Tag models (#5)

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,19 @@
 import type { CATEGORY } from "./constants";
 
 export type CategoryType = (typeof CATEGORY)[number];
+
+export interface Note {
+  noteId: string;
+  title: string;
+  createAt: string;
+  lastModified: string;
+  tags: string[];
+  isArchived: boolean;
+  content: string;
+}
+
+export interface Tag {
+  tagId: string;
+  tagName: string;
+  items: Record<string, string>[];
+}
