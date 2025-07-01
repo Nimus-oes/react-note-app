61d33cc6d68cb0e0c90c5a69a5df25f8c03b8426
Nimus-oes <sseo.code@gmail.com>
2025-07-01 15:20:40 +0900

    Move page title for conditional rendering on mobile (#4)
    Page title is moved from top level App component to the sub option panel on mobile so it is rendered only when it is a list page, instead of the content.

--- src/layout/root-layout-mobile.tsx
+++ src/layout/root-layout-mobile.tsx
@@ -1,14 +1,11 @@
 import { Outlet } from "react-router";
-import usePageTitle from "../hooks/use-page-title";
 import AppHeader from "./navigation/app-header";
 import NavBottomMobile from "./navigation/nav-bottom-mobile";
 
 export default function RootLayoutMobile() {
-  const pgaeTitle = usePageTitle();
   return (
     <div>
       <AppHeader />
-      <div>{pgaeTitle}</div>
       <Outlet />
       <NavBottomMobile />
     </div>
--- src/layout/sub-layout.tsx
+++ src/layout/sub-layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet, useParams } from "react-router";
 import { useIsMobile } from "../hooks/use-is-mobile";
 import styles from "./sub-layout.module.css";
+import usePageTitle from "../hooks/use-page-title";
 
 // This component is a generic layout for sub options. Any JSX node can be provided to be rendered.
 interface SubOptionProps {
@@ -10,9 +11,17 @@ interface SubOptionProps {
 export default function SubLayout({ subOptions }: SubOptionProps) {
   const { noteId } = useParams();
   const isMobile = useIsMobile();
+  const pageTitle = usePageTitle();
 
   // If noteId, it means it's a content page which will be rendered through Outlet
-  const mobileLayout = noteId ? <Outlet /> : <div>{subOptions}</div>;
+  const mobileLayout = noteId ? (
+    <Outlet />
+  ) : (
+    <>
+      <div>{pageTitle}</div>
+      <div>{subOptions}</div>
+    </>
+  );
   const desktopLayout = (
     <div className={styles.desktopContainer}>
       <div>{subOptions}</div>
