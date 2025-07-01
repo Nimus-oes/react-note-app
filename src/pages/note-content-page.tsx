871962c87f62796f77707e1f3652cbb3c80d8c98
Nimus-oes <sseo.code@gmail.com>
2025-07-01 15:18:05 +0900

    Add button panels for desktop/mobile screens (#4)

--- src/pages/note-content-page.tsx
+++ src/pages/note-content-page.tsx
@@ -1,7 +1,37 @@
+import { useIsMobile } from "../hooks/use-is-mobile";
+import styles from "./note-content-page.module.css";
+
 export default function NoteContentPage() {
-  return (
+  const isMobile = useIsMobile();
+
+  const mobileLayout = (
     <div>
+      <div className={styles.mobileButtonWrapper}>
+        <div>Go Back</div>
+        <div>Delete</div>
+        <div>Archive/Restore</div>
+        <div>Cancel</div>
+        <div>Save Note</div>
+      </div>
       <h2>Note Content Page</h2>
     </div>
   );
+
+  const desktopLayout = (
+    <div className={styles.container}>
+      <div className={styles.rightPanelWrapper}>
+        <h2>Note Content Page</h2>
+        <div className={styles.bottomButtonWrapper}>
+          <div>Save Note</div>
+          <div>Cancel</div>
+        </div>
+      </div>
+      <div>
+        <div>Archive/Restore Note</div>
+        <div>Delete Note</div>
+      </div>
+    </div>
+  );
+
+  return <>{isMobile ? mobileLayout : desktopLayout}</>;
 }
